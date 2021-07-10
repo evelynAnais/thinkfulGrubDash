790ec7d29bf42afb13bb6a286d730242e3c7d6cc
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-07-09 19:30:03 -0600

    delete functionality

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -42,14 +42,49 @@ function orderExists(req, res, next) {
   const foundOrder = orders.find((order) => order.id === orderId);
   if (foundOrder) { 
     res.locals.order = foundOrder
-    return next();
+    next();
   }
   next({
     status: 404,
     message: `dish id not found: ${req.params.orderId}`,
   });
 }
-
+// middleware
+function validateId(req, res, next) {
+  const orderId = req.params.orderId;
+  const { data: { id } } = req.body
+  if(id) {
+      if (orderId !== id) {
+      next({
+        status: 400,
+        message: `Dish id does not match route id. Dish: ${id}, Route: ${orderId}`,
+      });
+    }
+  }
+  next();
+}
+// middleware
+function validateStatus(req, res, next) {
+  const { data: { status } = {} } = req.body;
+  const validStatuses = ['delivered', 'out-for-delivery', 'pending']
+  if (!status) {
+    return res.status(400).json({ error: 'order must have a status' });
+  }
+  if (status === 'delivered') {
+    return res.status(400).json({ error: 'delivered order cannot be updated' });
+  }
+  if (!validStatuses.includes(status)) {
+    return res.status(400).json({ error: 'status invalid' });
+  }
+  next();
+}
+// middleware
+function validatePendingStatus(req, res, next) {
+  if (res.locals.order.status === 'pending') {
+    next();
+  }
+  return res.status(400).json({ error: 'an order cannot be deleted unless it is pending' });
+}
 
 function list(req, res) {
   res.json({ data: orders })
@@ -65,7 +100,6 @@ function create(req, res) {
     dishes: [...dishes]
   };
   orders.push(newOrder);
-  console.log('create order',newOrder)
   res.status(201).json({ data: newOrder });
 }
 
@@ -85,9 +119,19 @@ function update(req, res) {
   res.json({ data: updatedOrder });
 }
 
+function destroy(req, res) {
+  const { orderId } = req.params;
+  const index = orders.findIndex((order) => order.id === orderId);
+  if (index > -1) {
+    orders.splice(index, 1);
+  }
+  res.sendStatus(204);
+}
+
 module.exports = {
   list,
   create: [hasRequiredFields, validateDishes, validateQuantity, create],
   read: [orderExists, read],
-  update: [orderExists, hasRequiredFields, validateDishes, validateQuantity, validateId, update],
+  update: [orderExists, hasRequiredFields, validateDishes, validateQuantity, validateId, validateStatus, update],
+  delete: [orderExists, validatePendingStatus, destroy],
 }
